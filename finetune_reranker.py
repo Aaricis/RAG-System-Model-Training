9399b6ec1260d6cd1dd0e25bc28124c1a043ac87
Aaricis <947165603@qq.com>
2026-01-18 19:21:58 +0800

    feat: mining hard negatives, trainer, 训练参数

--- finetune_reranker.py
+++ finetune_reranker.py
@@ -8,175 +8,23 @@ from typing import Union, List, Dict
 
 from datasets import Dataset
 from sentence_transformers import LoggingHandler
+from sentence_transformers import SentenceTransformer
 from sentence_transformers.cross_encoder import CrossEncoder, CrossEncoderTrainingArguments, CrossEncoderTrainer
 from sentence_transformers.cross_encoder.evaluation import CrossEncoderRerankingEvaluator
 from sentence_transformers.cross_encoder.losses import BinaryCrossEntropyLoss
 from sentence_transformers.training_args import BatchSamplers
+from sentence_transformers.util import mine_hard_negatives
 
+# 设定日志
+logging.basicConfig(
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    handlers=[LoggingHandler()],
+)
+logger = logging.getLogger(__name__)
+logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
 
-#
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--train_data_path", type=str, default="./data/train.txt")
-# parser.add_argument("--test_data_path", type=str, default="./data/test_open.txt")
-# parser.add_argument("--model_name", type=str, default="cross-encoder/ms-marco-MiniLM-L12-v2")
-# parser.add_argument("--max_length", type=int, default=512)
-# parser.add_argument("--batch_size", type=int, default=32)
-# parser.add_argument("--model_save_path", type=str, default="./output/reranker")
-# parser.add_argument("--num_epochs", type=int, default=3)
-# parser.add_argument("--evaluation_steps", type=int, default=500)
-# args = parser.parse_args()
-#
-# short_model_name = args.model_name.split("/")[-1]
-# model_save_path = os.path.join(args.model_save_path,
-#                                f'train_cross-encoder-bcel-{short_model_name}-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}')
-#
-# os.makedirs(model_save_path, exist_ok=True)
-#
-# # 设定日志
-# logging.basicConfig(
-#     format="%(asctime)s - %(message)s",
-#     datefmt="%Y-%m-%d %H:%M:%S",
-#     level=logging.INFO,
-#     handlers=[LoggingHandler()],
-# )
-# logger = logging.getLogger(__name__)
-# logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
-#
-#
-# # 加载训练数据
-# def load_train_data(file_path):
-#     """
-#     加载训练数据
-#     :param file_path: 文件路径
-#     :return: train dataset
-#     """
-#     queries, evidences, labels = [], [], []
-#     with open(file_path, "r", encoding="utf-8") as f:
-#         for line in f:
-#             item = json.loads(line)
-#             query = item["rewrite"]
-#             for evidence, label in zip(item["evidences"], item["retrieval_labels"]):
-#                 queries.append(query)
-#                 evidences.append(evidence)
-#                 labels.append(float(label))
-#
-#     data = {
-#         "sentence1": queries,
-#         "sentence2": evidences,
-#         "label": labels
-#     }
-#
-#     pos_count = sum(1 for label in labels if label == 1.0)
-#     neg_count = len(labels) - pos_count
-#     logging.info(f"已加载 {len(labels)} 笔训练资料，正：{pos_count} / 负：{neg_count} (1:{neg_count / pos_count:.2f})")
-#     return Dataset.from_dict(data)
-#
-#
-# def load_test_data(file_path):
-#     """
-#     加载测试数据
-#     :param file_path: 文件路径
-#     :return: test dataset
-#     """
-#     samples = []
-#     with open(file_path, "r", encoding="utf-8") as f:
-#         for line in f:
-#             if not line.strip():
-#                 continue
-#             item = json.loads(line)
-#             if 1 not in item["retrieval_labels"]:  # 没有正例
-#                 continue
-#             query = item["rewrite"]
-#             positives = []
-#             negatives = []
-#             for evidence, label in zip(item["evidences"], item["retrieval_labels"]):
-#                 if label:
-#                     positives.append(evidence)
-#                 else:
-#                     negatives.append(evidence)
-#
-#             # 每个样本是一个三元组：查询，正例文档，负例文档列表
-#             samples.append(
-#                 {
-#                     'query': query,
-#                     'positive': positives,
-#                     'negative': negatives
-#                 }
-#             )
-#     logging.info(f'已加载 {len(samples)} 笔测试资料')
-#     return samples
-#
-#
-# # 1. 数据准备
-#
-# ## 训练集
-# train_dataset = load_train_data(args.train_data_path)
-#
-# ## 测试集
-# eval_samples = load_test_data(args.test_data_path)
-#
-# # 2. 加载模型
-# model = CrossEncoder(model_name_or_path=args.model_name, num_labels=1, max_length=args.max_length)
-#
-# # 3. 定义损失函数
-# loss = BinaryCrossEntropyLoss(model)
-#
-# # 4. 评估器
-# reranking_evaluator = CrossEncoderRerankingEvaluator(
-#     samples=eval_samples,
-#     name="test_rerank",
-#     batch_size=args.batch_size,
-#     show_progress_bar=True
-# )
-#
-# metrics = reranking_evaluator(model)
-# logging.info(f"Reranker模型微调前:{metrics}")
-#
-#
-# # 5. 定义训练参数
-#
-# training_args = CrossEncoderTrainingArguments(
-#     # Required parameter:
-#     output_dir=model_save_path,
-#     # Optional training parameters:
-#     num_train_epochs=args.num_epochs,
-#     per_device_train_batch_size=args.batch_size,
-#     per_device_eval_batch_size=args.batch_size,
-#     learning_rate=2e-5,
-#     warmup_ratio=0.1,
-#     fp16=False,  # Set to False if you get an error that your GPU can't run on FP16
-#     bf16=False,  # Set to True if you have a GPU that supports BF16
-#     load_best_model_at_end=True,
-#     # Optional tracking/debugging parameters:
-#     eval_strategy="steps",
-#     eval_steps=args.evaluation_steps,
-#     save_strategy="steps",
-#     save_steps=args.evaluation_steps,
-#     save_total_limit=2,
-#     logging_steps=50,
-#     logging_first_step=True,
-#     seed=12,
-#
-#     report_to=["tensorboard"],
-#     # 关键修改：指定实际存在的指标
-#     metric_for_best_model="eval_test_rerank_mrr@10",  # ✅ 使用 MRR@10 作为评估指标
-#
-#     # 重要：对于 MRR/MAP，越大越好，需设置 greater_is_better=True
-#     greater_is_better=True,
-# )
-#
-# # 6. 创建Trainer并开启训练
-# trainer = CrossEncoderTrainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=train_dataset,
-#     evaluator=reranking_evaluator,
-#     loss=loss
-# )
-#
-# logging.info("开始使用CrossEncoderTrainer训练！")
-# trainer.train()
-# logger.info("训练完成！")
 
 def load_data(file_path: str, is_single_object: bool = False) -> Union[List[Dict], Dict]:
     """
@@ -244,7 +92,62 @@ def prepare_train_examples(args):
         "sentence2": evidences,
         "label": labels
     }
-    return train_examples
+    return Dataset.from_dict(train_examples)
+
+
+def mine_negatives(args):
+    """
+    Add hard negatives to a dataset of (anchor, positive) pairs.
+    """
+
+    # 1. Load train dataset
+    train_data = load_data(args.train_file, is_single_object=False)
+
+    train_dataset = []
+    for item in train_data:
+        query = item.get('rewrite', '').strip()
+
+        evidences = item.get('evidences', [])
+        labels = item.get('retrieval_labels', [])
+
+        pos = None
+        for i, label in enumerate(labels):
+            if label == 1:
+                pos = evidences[i].strip()
+
+        if pos:
+            train_dataset.append({
+                "query": f'query: {query}',  # Apply E5 prefix
+                "positive": [f'passage: {pos}']  # Apply E5 prefix
+            })
+
+    # 2. Load Corpus (passages)
+    corpus_data = load_data(args.corpus_file, is_single_object=False)
+    corpus = []
+    for item in corpus_data:
+        text = item.get('text', '').strip()
+        pid = item.get('id')
+        if pid and text:
+            corpus.append(f"passage: {text}")  # Apply E5 prefix
+
+    # 3. Mining hard negatives
+    model = SentenceTransformer(args.retriever_model_path, device='cuda')
+
+    hard_dataset = mine_hard_negatives(
+        model=model,
+        dataset=Dataset.from_list(train_dataset),
+        corpus=corpus,
+        anchor_column_name="query",
+        positive_column_name="positive",
+        num_negatives=4,
+        sampling_strategy="top",
+        relative_margin=0.05,
+        batch_size=args.batch_size,
+        use_faiss=True,
+        output_format="labeled-pair" # Apply for BinaryCrossEntropyLoss
+    )
+
+    return hard_dataset
 
 
 def prepare_reranker_evaluator(args):
@@ -321,6 +224,7 @@ def prepare_reranker_evaluator(args):
         at_k=10,
         name="test_eval",
         batch_size=args.eval_batch_size,
+        always_rerank_positives=False,
     )
 
 
@@ -361,9 +265,9 @@ def fine_tune_reranker(args):
         load_best_model_at_end=True,
         # Optional tracking/debugging parameters:
         eval_strategy="steps",
-        eval_steps=args.evaluation_steps,
+        eval_steps=args.eval_steps,
         save_strategy="steps",
-        save_steps=args.evaluation_steps,
+        save_steps=args.eval_steps,
         save_total_limit=2,
         logging_steps=50,
         logging_first_step=True,
@@ -371,11 +275,31 @@ def fine_tune_reranker(args):
 
         report_to=["tensorboard"],
         # 关键修改：指定实际存在的指标
-        metric_for_best_model="eval_test_rerank_mrr@10",  # ✅ 使用 MRR@10 作为评估指标
+        metric_for_best_model="eval_test_eval_mrr@10",  # ✅ 使用 MRR@10 作为评估指标
 
         # 重要：对于 MRR/MAP，越大越好，需设置 greater_is_better=True
         greater_is_better=True,
     )
+
+    # 6. Train the Model
+    trainer = CrossEncoderTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_examples,
+        loss=train_loss,
+        evaluator=evaluator
+    )
+
+    logger.info("\nStarting fine-tuning...")
+    trainer.train()
+    logger.info(f"\nFine-tuning complete. Best model saved to {output_dir}")
+
+    # Evaluate the final model
+    metrics_after = evaluator(model)
+    logging.info(f'Metrics After: {metrics_after}')
+
+    # Save the final model
+    model.save_pretrained(output_dir)
 
 
 if __name__ == "__main__":
@@ -384,6 +308,8 @@ if __name__ == "__main__":
 
     parser.add_argument("--model_name_or_path", type=str, default="cross-encoder/ms-marco-MiniLM-L12-v2",
                         help="Pretrained cross-encoder model to load.")
+    # parser.add_argument("--retriever_model_path", type=str, required=True,
+    #                     help="Retriever model to load.")
 
     # --- Data Path Arguments ---
     parser.add_argument("--output_dir", type=str, default="./output/reranker",
@@ -410,4 +336,10 @@ if __name__ == "__main__":
                         help="The initial learning rate for AdamW.")
     parser.add_argument("--use_fp16", action="store_true",
                         help="Whether to use 16-bit precision (mixed precision).")
+    parser.add_argument("--eval_steps", type=int,
+                        default=100, help="Evaluation steps for evaluator")
     args = parser.parse_args()
+
+    # hard_negatives = mine_negatives(args)
+
+    fine_tune_reranker(args)
